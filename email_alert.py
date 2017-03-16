c0b633c7c522a3d8119b58785db4a58e3f56cf5a
Recall <root@recall.subnet88>
2017-03-15 21:27:43 -0500

    Fix email command to be functional

--- email_alert.py
+++ email_alert.py
@@ -1,1 +1,8 @@
-from os import subprocess
+#!/usr/local/bin/python3.6
+import subprocess
+from os import devnull
+
+def email_alert(recipient, from_name, subject, message):
+    subprocess.run("echo \"" + message + "\" | mail -s \"" + subject + "\" " + recipient,
+    shell=True)
+    return
