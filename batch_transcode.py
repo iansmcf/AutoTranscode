acb98a1209b5b6669a11e3d8de5793e7b11ef56c
Ian McFarlane <iansmcfarlane@gmail.com>
2017-03-15 22:00:39 -0500

    First functional version, add email alerts and remove comments from transcode

--- batch_transcode.py
+++ batch_transcode.py
@@ -2,6 +2,8 @@
 import subprocess
 from os import listdir,path,devnull
 from os.path import isfile,join
+from email_alert import email_alert
+import time
 
 
 ## definitions
@@ -28,19 +30,34 @@ output_folder="/general/fs1/transcode_output/"
 # libx264 encoder options
 encode_crf="16"
 encode_preset="ultrafast"
+# email target
+recipient_email = open('./email.txt', 'r').read()
+sender_name="Recall"
+timestamp=time.strftime('%b %d, %H:%M')
 
 ## main
 # get files to process
 input_files=listdir(input_folder)
 
 # tell user what we will do
+alert_string="\n"
+
+
 print("Transcoding the following files:")
 for entry in input_files:
     print(" - ",join(input_folder,entry))
+    # also add the files to the list
+    alert_string=alert_string+entry+"\n"
+
+# send email about the start of the transcode
+email_alert(recipient_email,sender_name,"Transcode starting " + timestamp,
+            "Beginning transcode for the following files:\n"+alert_string)
 
 # process files
-#for input_file in input_files:
-    #transcode_file(join(input_folder,input_file), join(output_folder,input_file),
-    #encode_crf, encode_preset)
+for input_file in input_files:
+    transcode_file(join(input_folder,input_file), join(output_folder,input_file),
+    encode_crf, encode_preset)
 
 print("Transcoding complete.")
+email_alert(recipient_email,sender_name,"Transcode starting " + timestamp,
+            "Finished transcode for the following files:\n"+alert_string)
