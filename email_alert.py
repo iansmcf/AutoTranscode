f170f4a4b307aedcb78efd6e6ca76293eb432f49
Commoner <commoner@recall.subnet88>
2017-03-15 20:50:57 -0500

    Change email strategy

--- email_alert.py
+++ email_alert.py
@@ -1,13 +1,1 @@
-import smtplib
-
-from email.mime.text import MIMEtext
-
-
-# construct message
-#msg['Subject'] = "Test alert"
-#msg['From'] = "root"
-#msg['To'] = "iansmcfarlane@gmail.com"
-
-s = smtplib.SMTP('smtp.gmail.com')
-s.sendmail("root","iansmcfarlane@gmail.com","aeuausd")
-s.quit()
+from os import subprocess
